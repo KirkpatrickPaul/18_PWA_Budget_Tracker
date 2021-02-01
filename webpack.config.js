d4b02e1561dcf5f892d5303628f77196d2a864e4
Paul Kirkpatrick <RadicalTurnip@gmail.com>
2021-01-31 21:08:11 -0600

    restructures file structure and adds finished webpack.conig.js

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const path = require('path');
+
+const config = {
+  entry: './assets/js/index.js',
+  output: {
+    path: __dirname + '/public/dist',
+    filename: 'bundle.js'
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new WebpackPwaManifest({
+      fingerprints: false,
+      name: 'Budget Tracker App',
+      short_name: 'Budget',
+      description:
+        'An application that allows you to track your budget by adding or subtracting money.',
+      background_color: '#01579b',
+      theme_color: '#317EFB',
+      'theme-color': '#317EFB',
+      start_url: '/',
+      icons: [
+        {
+          src: path.resolve('assets/icons/icon.png'),
+          sizes: [192, 512],
+          destination: path.join('assets', 'icons')
+        }
+      ]
+    })
+  ]
+};
+
+module.exports = config;
