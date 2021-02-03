1c93e0c5fbf2c646c774652976be95a49bf400c0
Paul Kirkpatrick <RadicalTurnip@gmail.com>
2021-02-02 22:55:09 -0600

    finishes service worker sans indexedDB.

--- public/assets/js/service-worker.js
+++ public/assets/js/service-worker.js
@@ -21,4 +21,54 @@ self.addEventListener('install', (event) => {
   );
 });
 
+self.addEventListener('activate', event => {
+  const 
+})
 
+self.addEventListener("activate", function(event) {
+  event.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(
+        keyList.map(key => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            console.log("Removing old cache data", key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", function(evt) {
+  const {url} = evt.request;
+  if (url.includes("/api/")) {
+    evt.respondWith(
+      caches.open(DATA_CACHE_NAME).then(cache => {
+        return fetch(evt.request)
+          .then(response => {
+            // If the response was good, clone it and store it in the cache.
+            if (response.status === 200) {
+              cache.put(evt.request, response.clone());
+            }
+
+            return response;
+          })
+          .catch(err => {
+            return cache.match(evt.request);
+          });
+      }).catch(err => console.log(err))
+    );
+  } else {
+    // respond from static cache, request is not for /api/*
+    evt.respondWith(
+      caches.open(CACHE_NAME).then(cache => {
+        return cache.match(evt.request).then(response => {
+          return response || fetch(evt.request);
+        });
+      })
+    );
+  }
+});
